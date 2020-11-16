c0c3990cf09502c683015e2679fdc120017d942d
F-Cup-Chan <bringonthepretty@gmail.com>
2020-11-16 23:36:03 +0300

    upd

--- app/src/main/java/com/fcupchan/zmeika/Food.kt
+++ app/src/main/java/com/fcupchan/zmeika/Food.kt
@@ -8,37 +8,38 @@ class Food {
     companion object {
         var isFoodExists = false
 
+        private val random = Random
+        var positionFoodX = BigDecimal.valueOf(random.nextDouble(-0.7, 0.7)).setScale(1).toDouble()
+        var positionFoodY = BigDecimal.valueOf(random.nextDouble(-1.3, 1.3)).setScale(1).toDouble()
+
 
         fun createFood(){
-            val random = Random
-            var positionX = BigDecimal.valueOf(random.nextDouble(-0.7, 0.7)).setScale(1).toDouble()
-            var positionY = BigDecimal.valueOf(random.nextDouble(-1.3, 1.3)).setScale(1).toDouble()
-            //positionX = BigDecimal.valueOf(positionX).setScale(1).toDouble()
-            //positionY = BigDecimal.valueOf(positionY).setScale(1).toDouble()
             var bool = true
-
             while (bool){
                 bool = false
                 Snake.segments.forEach{
-                    if (it.centerX == positionX.toFloat()){
+                    if (it.centerX == positionFoodX.toFloat()){
                         bool = true
-                        positionX = BigDecimal.valueOf(random.nextDouble(-0.7, 0.7)).setScale(1).toDouble()
+                        positionFoodX = BigDecimal.valueOf(random.nextDouble(-0.7, 0.7)).setScale(1).toDouble()
                         return@forEach
                     }
                 }
             }
-
             bool = true
             while (bool){
                 bool = false
                 Snake.segments.forEach{
-                    if (it.centerY == positionY.toFloat()){
+                    if (it.centerY == positionFoodY.toFloat()){
                         bool = true
-                        positionY = BigDecimal.valueOf(random.nextDouble(-1.3, 1.3)).setScale(1).toDouble()
+                        positionFoodY = BigDecimal.valueOf(random.nextDouble(-1.3, 1.3)).setScale(1).toDouble()
                         return@forEach
                     }
                 }
             }
+        }
+
+        fun drawFood(matrix: FloatArray){
+
         }
     }
 }
